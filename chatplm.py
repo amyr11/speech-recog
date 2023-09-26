cb09eb85c855b812a360fd8fb85e83df96b025be
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-09-27 01:58:51 +0800

    First commit

---
+++ chatplm.py
@@ -0,0 +1,28 @@
+import requests
+import speech_recognition
+
+def getresponse(prompt):
+    url = "https://chatplm-api.onrender.com/chat"
+    params = {
+        "prompt": prompt
+    }
+    headers = {
+        "api-key": "chatplm-api-bscs22"
+    }
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()["output"]
+
+
+recognizer = speech_recognition.Recognizer()
+with speech_recognition.Microphone() as source:
+    print("Say something!")
+    audio = recognizer.listen(source)
+
+text = recognizer.recognize_google(audio)
+output = getresponse(text)
+
+print("[You]:")
+print(text)
+print()
+print("[ChatPLM]:")
+print(output)
